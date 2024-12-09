de8ff1c73f9bd0c1da9990835c62a867c84d5bd1
guilherme_git <guilhermeloli.dev@gmail.com>
2024-12-09 14:20:35 -0300

    fix cors

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,25 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Access-Control-Allow-Origin',
+          value: 'https://smart-save-app.vercel.app',
+        },
+        {
+          key: 'Access-Control-Allow-Methods',
+          value: 'GET, POST, PUT, DELETE, OPTIONS',
+        },
+        {
+          key: 'Access-Control-Allow-Headers',
+          value: 'Content-Type, Authorization',
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
