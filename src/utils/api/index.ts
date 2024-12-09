312941161452d2cc09d869d576f7d4064de00cb0
guilherme_git <guilhermeloli.dev@gmail.com>
2024-12-09 14:29:40 -0300

    fix url production

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,5 +1,15 @@
 import axios from 'axios';
 
+const baseUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : process.env.NEXT_PUBLIC_URL;
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: `${baseUrl}/api`,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  },
 });
