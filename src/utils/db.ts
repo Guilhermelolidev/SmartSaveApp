ca0fb5bedae5ae3b7d46d430f4a71272fd2a5bb6
guilherme_git <guilhermeloli.dev@gmail.com>
2024-12-09 01:25:44 -0300

    fix db config

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,13 @@
 import { PrismaClient } from '@prisma/client';
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const db = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
+export const db = new PrismaClient();
+async function main() {
+  //await db.user.deleteMany();
+}
+main()
+  .catch(async e => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await db.$disconnect();
+  });
